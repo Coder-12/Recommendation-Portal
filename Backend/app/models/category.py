021dc1aad771f815fab5085cfeb3f3dd3e2bb73d
Coder-12 <31615917+Coder-12@users.noreply.github.com>
2020-12-13 17:48:03 +0530

    Add files via upload

---
+++ Backend/app/models/category.py
@@ -0,0 +1,54 @@
+from enum import Enum
+from typing import List, Optional
+
+from pydantic import AnyUrl, Field
+
+from .base import Base, ObjectID, MusicEnum, VideoEnum, CategoryEnum
+
+
+class Category(Base):
+    """Category definiton"""
+    id: CategoryEnum = Field(None, alias="_id")
+
+
+class ItemBase(Base):
+    """Base fields for any item"""
+    id: ObjectID = Field(None, alias="_id")
+    flags: List[str] = list()
+    hidden: bool = False
+    title: str
+    link: AnyUrl
+    year_release: int
+    genres: List[str] = list()
+
+
+class Movie(ItemBase):
+    """Movie category definition"""
+    language: Optional[str]
+    director: Optional[List[str]]
+    streaming: Optional[List[VideoEnum]]
+
+
+class Show(ItemBase):
+    """Shows category definition"""
+    seasons: Optional[int]
+    episode_length: Optional[int]
+    season_length: Optional[int]
+    streaming: Optional[List[VideoEnum]]
+
+
+class Anime(Show):
+    """Anime category defintion"""
+    # NOTE update if any other fields required
+    pass
+
+
+class Music(ItemBase):
+    """Music category definiton"""
+    artist: Optional[str]
+    album: Optional[str]
+    streaming: Optional[List[MusicEnum]]
+
+
+class Book(ItemBase):
+    author: Optional[str]
