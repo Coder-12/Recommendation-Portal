93c7d4f698b10f4f173681b88c61545fb0184b94
Coder-12 <31615917+Coder-12@users.noreply.github.com>
2020-12-13 17:48:39 +0530

    Add files via upload

---
+++ Backend/app/models/base.py
@@ -0,0 +1,78 @@
+from datetime import datetime, timezone
+from enum import IntEnum, Enum
+
+from pydantic import BaseConfig, BaseModel
+from fastapi import HTTPException
+from bson.objectid import ObjectId
+
+
+class CategoryEnum(str, Enum):
+    anime = 'anime'
+    books = 'books'
+    movies = 'movies'
+    music = 'music'
+    shows = 'shows'
+
+
+class MusicEnum(str, Enum):
+    youtube = 'youtube'
+    spotify = 'spotify'
+    gaana = 'gaana'
+    prime = 'prime'
+    apple = 'apple'
+    saavn = 'saavn'
+
+
+class VideoEnum(str, Enum):
+    netflix = 'netflix'
+    hotstar = 'hotstar'
+    torrent = 'torrent'
+    prime = 'prime'
+    youtube = 'youtube'
+    other = 'other'
+
+
+class LikeEnum(IntEnum):
+    """Like options"""
+    dislike = -1
+    like = 1
+
+
+class RatingEnum(IntEnum):
+    """Rating options"""
+    very_horrible = 1
+    horrible = 2
+    very_bad = 3
+    bad = 4
+    neutral = 5
+    decent = 6
+    good = 7
+    very_good = 8
+    excellent = 9
+    amazing = 10
+
+
+class ObjectID(str):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(str(v)):
+            raise HTTPException(status_code=400, detail=f"Not a valid ID {v}")
+        return ObjectId(str(v))
+
+
+class Base(BaseModel):
+    class Config(BaseConfig):
+        allow_population_by_alias = True
+        arbitrary_types_allowed = True
+        json_encoders = {
+            datetime: lambda dt: dt.replace(tzinfo=timezone.utc)
+            .isoformat()
+            .replace("+00:00", "Z"),
+            ObjectId: ObjectID,
+            LikeEnum: lambda lE: lE.value,
+            RatingEnum: lambda rE: rE.value
+        }
